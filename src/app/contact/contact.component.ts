8afbedaea01dd83b684bd862df993d501e903454
mic-pic <pichlermichaela@outlook.com>
2024-07-02 11:32:34 +0200

    contact form validation added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validator, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    phone: new FormControl('', Validators.required),
+    message: new FormControl('', Validators.required),
+});
 
+onSubmit() {
+  if (this.contactForm.valid) {
+  console.warn(this.contactForm.value);
+  this.contactForm.reset();
+};
+};
 }
