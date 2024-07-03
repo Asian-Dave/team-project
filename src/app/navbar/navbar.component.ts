8b3d3e8da674442b3d15a09501849e97d81693d9
Asian-Dave <dct.saocr688@gmail.com>
2024-07-03 17:34:02 +0200

    Sllider added, Navbar animation fixed with the specified things. sma adjustments made

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,34 +1,38 @@
-// navbar.component.ts
-import { Component, HostListener, OnInit, DoCheck } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit, DoCheck{
+export class NavbarComponent implements OnInit {
   isScrolled = false;
+  isHomePage = false;
 
-  constructor(private route: Router) {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isHomePage = event.url === '/' || event.urlAfterRedirects === '/';
+        if (this.isHomePage) {
+          // Check the current scroll position and set isScrolled accordingly
+          const offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+          this.isScrolled = offset > 57;
+        } else {
+          // Ensure the navbar has the scrolled color on non-homepages
+          this.isScrolled = true;
+        }
+      }
+    });
   }
 
-  ngDoCheck(): void {
-    console.log(this.route.url);
-    if (this.route.url == "/") {
-      
-    }
-  }
-
- ngOnInit(): void {
-   
- }
-  
   @HostListener('window:scroll', [])
   onWindowScroll(): void {
-    const offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    this.isScrolled = offset > 57;
+    if (this.isHomePage) {
+      const offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      this.isScrolled = offset > 57;
+    }
   }
 }
-
-
