3ad4d01089865d574affbd35053e4b885845429b
mic-pic <pichlermichaela@outlook.com>
2024-07-03 10:25:24 +0200

    contactForm reset

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -22,11 +22,10 @@ export class ContactComponent {
   constructor(private http: HttpClient) {}
   onSubmit() {
     if (this.contactForm.valid) {
-      this.contactForm.reset();
-
       this.http
         .post('https://formspree.io/f/mqazkprp', this.contactForm.value)
         .subscribe((response) => (this.msg = 'Thanks for your message!'));
     }
+    this.contactForm.reset();
   }
 }
